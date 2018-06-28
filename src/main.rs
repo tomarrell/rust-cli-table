f79c106f474b88e4eef01fbb3922012bdc042981
Tom Arrell <tom.arrell@gmail.com>
2018-06-28 15:36:20 +1200

    Calculate widest row for each column and format accordingly

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 extern crate clap;
 
-use std::iter;
-
-use clap::{App, Arg, SubCommand};
+use clap::App;
 
 fn main() {
-    let matches = App::new("rust-cli-table")
+    App::new("rust-cli-table")
         .version("0.1.0")
         .author("Tom A. <tom.arrell@gmail.com>")
         .about("Renders a table in the command line from a CSV")
@@ -14,35 +12,75 @@ fn main() {
     let test_data: &[&[&str]] = &[
         &["Name", "Email", "Address", "Phone"],
         &["Tom", "tom@tom.com", "2 Cool St", "02145718"],
+        &["James", "james@movio.com", "2 St", "0214"],
     ];
 
-    render_header(test_data.get(0).unwrap());
-    render_data(test_data.get(1..).unwrap());
+    let max_column_width_matrix = max_column_width(test_data);
+    println!("{:?}", max_column_width_matrix);
+
+    render_header(test_data.get(0).unwrap(), max_column_width_matrix.clone());
+    render_data(test_data.get(1..).unwrap(), max_column_width_matrix.clone());
 }
 
-fn render_data(data: &[&[&str]]) -> usize {
-    let mut data_string = String::from("");
+fn max_column_width<'a>(table: &'a [&[&str]]) -> Vec<usize> {
+    // Might panic
+    let col_count = table.get(0).unwrap().len();
 
-    for row in data {
-        for cell in row.iter() {
-            data_string.push_str(&format!("| {} ", cell));
+    let mut width_matrix = vec![0; col_count];
+
+    for row in table {
+        for (i, item) in row.iter().enumerate() {
+            if width_matrix.get(i).unwrap() < &item.len() {
+                width_matrix[i] = item.len();
+            }
         }
     }
 
-    println!("{}|", data_string);
-
-    1
+    width_matrix
 }
 
-fn render_header(headers: &[&str]) -> usize {
+fn render_data(data: &[&[&str]], max_width_matrix: Vec<usize>) -> usize {
+    let mut data_string = String::from("");
+
+    for row in data {
+        for (index, cell) in row.iter().enumerate() {
+            data_string.push_str(&format!(
+                "| {:width$} ",
+                cell,
+                width = *max_width_matrix.get(index).unwrap_or(&1)
+            ));
+        }
+        data_string.push_str("|\n");
+    }
+
+    print!("{}", data_string);
+
+    let abs_width = data_string
+        .as_str()
+        .split("\n")
+        .next()
+        .unwrap()
+        .chars()
+        .count();
+
+    print_spacer(abs_width);
+
+    abs_width
+}
+
+fn render_header(headers: &[&str], max_width_matrix: Vec<usize>) -> usize {
     let mut header_string = String::from("");
 
-    for h in headers.iter() {
-        header_string.push_str(&format!("| {} ", h));
+    for (index, h) in headers.iter().enumerate() {
+        header_string.push_str(&format!(
+            "| {:width$} ",
+            h,
+            width = *max_width_matrix.get(index).unwrap_or(&2)
+        ));
     }
 
     // Absolute length of printed line, including beginning and end symbols
-    let header_length = header_string.len() + 1;
+    let header_length = header_string.chars().count() + 1;
 
     print_spacer(header_length);
     println!("{}|", header_string);
@@ -52,5 +90,5 @@ fn render_header(headers: &[&str]) -> usize {
 }
 
 fn print_spacer(length: usize) {
-    println!("|{}|", "-".repeat(length - 2 as usize));
+    println!("+{}+", "-".repeat(length - 2 as usize));
 }
